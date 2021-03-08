a3b0d68758c5ba1037d1ff6430d4682903ada35c
Jacob White <jwhite.wizard@gmail.com>
2021-03-08 02:44:23 -0600

    remove unused imports

--- src/main/java/com/jwhitewizard/quotely/application/QuotelyApplication.java
+++ src/main/java/com/jwhitewizard/quotely/application/QuotelyApplication.java
@@ -1,21 +1,10 @@
 package com.jwhitewizard.quotely.application;
 
 import com.jwhitewizard.quotely.client.QuotelyClient;
-import com.jwhitewizard.quotely.domain.Quote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class QuotelyApplication {
