0e1412682c761e20d89a4346a98f8f3f63e3e193
Jacob White <jacobwhite@Jacobs-MacBook-Pro-2.local>
2021-03-08 02:32:21 -0600

    refactored project to use spring web client, broke the call out of the app and into a client class

--- src/main/java/com/jwhitewizard/quotely/application/QuotelyApplication.java
+++ src/main/java/com/jwhitewizard/quotely/application/QuotelyApplication.java
@@ -1,5 +1,6 @@
 package com.jwhitewizard.quotely.application;
 
+import com.jwhitewizard.quotely.client.QuotelyClient;
 import com.jwhitewizard.quotely.domain.Quote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,32 +21,42 @@ import java.util.Arrays;
 public class QuotelyApplication {
     private static final Logger log = LoggerFactory.getLogger(QuotelyApplication.class);
 
-    public static void main(String[] args) { SpringApplication.run(QuotelyApplication.class, args); }
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    public static void main(String[] args) {
+        SpringApplication.run(QuotelyApplication.class, args);
+        if (args.length == 0 || args.length == 1 && ("English".equalsIgnoreCase(args[0]))) {
+            log.info("English language accepted");
+            QuotelyClient client = new QuotelyClient();
+        } else if (args.length == 1 && ("Russian".equalsIgnoreCase(args[0]))) {
+            log.info("Russian language accepted");
+            QuotelyClient client = new QuotelyClient("ru");
+        }
+        System.exit(0);
     }
 
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
-        return args -> {
-            HttpHeaders headers = new HttpHeaders();
-            HttpEntity<String> entity = new HttpEntity<String>(headers);
-            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-            log.info("checking for language");
-//            If no argument is input or there's only one arg and it is english
-            if (args.length == 0 || args.length == 1 && ("English".equalsIgnoreCase(args[0]))){
-                log.info("English language accepted");
-                ResponseEntity<String> quote = restTemplate.exchange("https://api.forismatic.com/api/1.0/?method=" +
-                        "getQuote&format=json&lang=en", HttpMethod.GET, entity, String.class);
-                System.out.println(quote);
-            } else if (args.length == 1 && ("Russian".equalsIgnoreCase(args[0]))) {
-                log.info("Russian language accepted");
-                ResponseEntity<String> quote = restTemplate.exchange("https://api.forismatic.com/api/1.0/?method=" +
-                        "getQuote&format=json&lang=ru", HttpMethod.GET, entity, String.class);
-                System.out.println(quote);
-            }
-        };
-    }
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder.build();
+//    }
+//
+//    @Bean
+//    public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+//        return args -> {
+//            HttpHeaders headers = new HttpHeaders();
+//            HttpEntity<String> entity = new HttpEntity<String>(headers);
+//            headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+//            log.info("checking for language");
+////            If no argument is input or there's only one arg and it is english
+//            if (args.length == 0 || args.length == 1 && ("English".equalsIgnoreCase(args[0]))){
+//                log.info("English language accepted");
+//                ResponseEntity<String> quote = restTemplate.exchange("https://api.forismatic.com/api/1.0/?method=" +
+//                        "getQuote&format=json&lang=en", HttpMethod.GET, entity, String.class);
+//                System.out.println(quote);
+//            } else if (args.length == 1 && ("Russian".equalsIgnoreCase(args[0]))) {
+//                log.info("Russian language accepted");
+//                ResponseEntity<String> quote = restTemplate.exchange("https://api.forismatic.com/api/1.0/?method=" +
+//                        "getQuote&format=json&lang=ru", HttpMethod.GET, entity, String.class);
+//                System.out.println(quote);
+//            }
+//        };
+//    }
 }
---
+++ src/main/java/com/jwhitewizard/quotely/client/QuotelyClient.java
@@ -0,0 +1,33 @@
+package com.jwhitewizard.quotely.client;
+
+import com.jwhitewizard.quotely.domain.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class QuotelyClient {
+    private WebClient client;
+    private String language = "en";
+    private Mono<Quote> result;
+    private static final Logger log = LoggerFactory.getLogger(QuotelyClient.class);
+
+    public QuotelyClient() {
+        new QuotelyClient("en");
+    }
+
+    public QuotelyClient(String language){
+        log.info("preparing to retrieve a quote in " + ((language.equals("en")) ? "English" : "Russian"));
+        client = WebClient.create("https://api.forismatic.com");
+        result = client.get().uri(uriBuilder -> uriBuilder.path("/api/1.0/")
+                .queryParam("method", "getQuote")
+                .queryParam("format", "json")
+                .queryParam("lang", language)
+                .build()
+        ).retrieve().bodyToMono(Quote.class);
+        log.info("completed the call");
+        Quote quote = result.block();
+        System.out.println("\'" + quote.getQuoteText() + "\'");
+        System.out.println("- " + quote.getQuoteAuthor());
+    }
+}
